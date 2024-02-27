11c85494203cf6b107d83bedf7d3d23719b028f1
Karl Palmskog <palmskog@gmail.com>
2024-02-27 18:29:13 +0100

    add coq-stdpp as dependency, add abs_examples module (#12)
    * add coq-stdpp as dependency, add abs_examples module
    
    * Typing examples
    
    * nits
    
    ---------
    
    Co-authored-by: Aqissiaq <aqissiaq@gmail.com>

---
+++ theories/abs_examples.v
@@ -0,0 +1,37 @@
+From ABS Require Import abs_defs.
+From stdpp Require Import prelude base.
+
+(** * ABS Examples *)
+
+Definition e_const_5 : e := 
+  e_t (t_int 5%Z).
+
+Definition func_const_5 : F := 
+  F_fn T_int "const_5"%string [] e_const_5.
+
+Definition e_call_const_5 : e :=
+  e_fn_call "const_5"%string [].
+
+Definition Ctx : G :=
+  Map.add ("const_5"%string) (ctxv_sig (sig_sig [] T_int)) (Map.empty ctxv).
+
+Lemma e_const_5_T_int :
+ typ_e Ctx e_const_5 T_int.
+Proof. by apply typ_int. Qed.
+
+Lemma e_call_const_typ_F :
+ typ_F Ctx func_const_5.
+Proof.
+  apply typ_func_decl.
+  - easy.
+  - apply e_const_5_T_int.
+Qed.
+
+Lemma e_call_const_5_T_int :
+ typ_e Ctx e_call_const_5 T_int.
+Proof.
+  unfold e_call_const_5.
+  replace [] with (map (fun (pat_:(e*T)) => match pat_ with (e_,T_) => e_ end ) [])
+    by auto.
+  by apply typ_func_expr.
+Qed.
